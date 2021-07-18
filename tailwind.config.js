29a82b952c317d1330f79f22ccb9e5b1f44352d4
Nguyễn Đạt <thanhdat19521@gmail.com>
2021-07-18 22:27:36 +0700

    dat: update

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,14 +4,7 @@ module.exports = {
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
-    screens : {
-      '2xl': {'max': '1535px'},
-      'xl': {'max': '1279px'},
-      'lg': {'max': '1023px'},
-      'md': {'max': '767px'},
-      'sm': {'max': '639px'},
-    }
+    extend: {}
   },
   variants: {
     extend: {},
