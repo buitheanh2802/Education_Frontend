e1dd402a143b1beed564a7bc4658d97b9bef8d3c
Nguyễn Thành Đạt <73815905+thanhdat19521@users.noreply.github.com>
2021-07-19 14:57:47 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import store from './redux/store';
 import RootRouter from './routes';
+import "./assets/css/style.css";
 
 const App = () => {
   return (
--- src/components/Headers/index.jsx
+++ src/components/Headers/index.jsx
@@ -1,22 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link, NavLink } from 'react-router-dom';
 import Logo from '../../assets/media/pictures/logo.png';
 import Avatar from '../../assets/media/pictures/avatar.png';
-import Bell from '../../assets/media/icons/bell.svg';
+import IconBell from '../../assets/media/icons/bell.svg';
+import IconList from '../../assets/media/icons/list.svg';
+import IconClose from '../../assets/media/icons/cancel.svg';
 
 const Header = () => {
+    const [menu, setMenu] = useState(false);
     return (
-        <header className="py-[10px bg-[#000000] opacity-80">
+        <header className="py-[10px] bg-[#000000] bg-opacity-80 px-[15px]">
             <div className="flex justify-between items-center container mx-auto">
-                <Link to="/"><img src={Logo} alt="Eduford.com" /></Link> 
-                <ul className="text-[18px] text-[#ffffff] flex items-center">
+                <Link to="/"><img src={Logo} alt="Eduford.com" /></Link>
+                <button onClick={() => setMenu(true)} className="xl:hidden"><img className="w-[35px] h-[35px]" src={IconList} alt="" /></button>
+                <ul className={`${menu ? "block bg-[#000000] absolute top-0 right-0 bottom-0 left-0" : "hidden"} text-[18px] text-[#ffffff] xl:flex items-center`}>
+                    <li className="flex xl:hidden justify-between py-[10px] px-[15px]">
+                        <Link to="/"><img src={Logo} alt="Eduford.com" /></Link>
+                        <button onClick={() => setMenu(false)}><img className="w-[35px] h-[35px]" src={IconClose} alt="" /></button>
+                    </li>
                     <li className="px-[15px]"><NavLink activeClassName="text-[#ff0000]" to="/">Trang chủ</NavLink></li>
                     <li className="px-[15px]"><NavLink activeClassName="text-[#ff0000]" to="/khoa-hoc">Khóa học</NavLink></li>
                     <li className="px-[15px]"><NavLink activeClassName="text-[#ff0000]" to="/thuc-hanh">Thực hành</NavLink></li>
-                    <li className="px-[15px]">
+                    <li className="px-[15px] menu-before relative">
                         <NavLink to="/dang-nhap">Đăng nhập</NavLink>
                         <button className="hidden">
-                            <img className="w-[35px] h-[35px]" src={Bell} alt="thông báo" />
+                            <img className="w-[35px] h-[35px]" src={IconBell} alt="thông báo" />
                         </button>
                     </li>
                     <li className="pl-[15px]">
