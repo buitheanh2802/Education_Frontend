34c97a42b29ce99d312cc5b6861e614317084482
Kiều Hạnh Nguyên <kieuhanhnguyen01@gmail.com>
2021-07-23 22:14:51 +0700

    update

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,8 +1,8 @@
-import React from 'react'
+import React from 'react';
 
 const Header = () => {
     return (
-        <div>
+        <div className="text-[#ff0000]">
             đây là header
         </div>
     )
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   purge: [
-    "./src/**/*.jsx"
+    "./src/**/*.jsx",
+    "./src/**/*.js"
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
