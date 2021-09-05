504c5361a73f64155e734e43b7ef78f57481fa2e
Nguyễn Thành Đạt <73815905+thanhdat19521@users.noreply.github.com>
2021-09-05 22:49:43 +0700

    Add files via upload

---
+++ src/Constants/index.js
@@ -0,0 +1,29 @@
+export const path = {
+    // Website
+    HOME: '/',
+
+    // questions
+    QUESTIONS: '/questions',
+    QUESTIONS_CREATE: '/questions/create',
+    QUESTIONS_FLOW: '/questions/flow',
+    QUESTIONS_BOOK_MARK: '/questions/bookmark',
+
+    // post
+    POSTS: '/posts',
+    POSTS_ID: '/posts/:id',
+    POSTS_CREATE: '/posts/create',
+    POSTS_POPULAR: '/posts/popular',
+    POSTS_FLOW: '/posts/flow',
+    POSTS_BOOK_MARK: '/posts/bookmark',
+
+    // tags
+    TAGS: '/tags',
+    TAGS_ID: '/tags/:id',
+
+    // auth
+    LOGIN: '/login',
+    REGISTER: '/register',
+
+    // Admin
+    ADMIN: '/admin'
+}
