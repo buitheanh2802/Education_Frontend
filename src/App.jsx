fec2ffbb27a35ae86e4568dedba362c9b7d29a35
Nguyen Thi My My <myntmph12101@fpt.edu.vn>
2021-09-27 09:52:35 +0700

    update:TestAPI

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,33 @@
-import React from 'react';
-import { Provider } from 'react-redux';
-import Store from './Redux/Store';
-import RootRouter from './Routes';
+import React, { useEffect, useState } from "react";
+import { Provider } from "react-redux";
+import Store from "./Redux/Store";
+import RootRouter from "./Routes";
 import "./Assets/css/main.css";
+import TagAPi from "./Apis/TagApi";
 
 const App = () => {
+  const [tags, setTag] = useState([]);
+  // useEffect(() => {
+  //   // fetch("https://devstar-mockapi.herokuapp.com/tags")
+  //   //   .then((abc) => abc.json())
+  //   //   .then((abc) => {
+  //   //     console.log(abc);
+  //   //   });
+  //   // const fetchData = async () => {
+  //   //   try {
+  //   //     const { data: tags } = await TagAPi.getAll();
+  //   //     setTag(tags);
+  //   //   } catch (error) {
+  //   //     console.log(error);
+  //   //   }
+  //   // };
+  //   fetchData();
+  //   console.log(tags)
+  // }, []);
+
   return (
-    <Provider store={Store}>
+    <Provider store={Store}
+    >
       <RootRouter />
     </Provider>
   );
