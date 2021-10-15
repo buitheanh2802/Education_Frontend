28ca22030a212e9252c6614f7973592fccda7942
vinhtran01 <vinhtq030901@gmail.com>
2021-10-15 09:38:39 +0700

    vinh:update

--- src/Pages/Public/EditProfile/index.jsx
+++ src/Pages/Public/EditProfile/index.jsx
@@ -12,86 +12,98 @@ const EditProfile = () => {
 
   return (
     <>
-      <div className=" mx-auto mt-[75px] ">
-        <div className="flex mt-10">
-          <div className=" lg:w-[20%] bg-white shadow-lg rounded-[3px] 2xl:h-[90vh]">
-            <div className="w-full">
-              <div className="flex items-center py-[17px] hover:bg-blue-100 px-[20px]">
-                <Icon.Home className="fill-current w-[25px] text-gray-400 " />
-                <p className="ml-3 text-[15px] hidden lg:block">Trang Chủ</p>
-              </div>
-            </div>
-            <div className="w-full relative ">
-              <div
-                className="flex items-center justify-between py-[17px] hover:bg-blue-100 px-[20px]"
-                onClick={() => setBoxInfo(!BoxInfo)}
-              >
-                <div className="flex items-center">
-                  <Icon.Card className="fill-current w-[23px] text-gray-400" />
-                  <p className="ml-3 text-[15px] hidden lg:block">Thông Tin</p>
+      <div className=" mx-auto  ">
+        <div className="flex ">
+          <div className="lg:w-[20%]   md:h-[100vh] pt-[75px]">
+            <div className=" bg-white h-full shadow-lg rounded-[3px]">
+              <div className="w-full">
+                <div className="flex items-center py-[17px] hover:bg-blue-100 px-[20px]">
+                  <Icon.Home className="fill-current w-[25px] text-gray-400 " />
+                  <p className="ml-3 text-[15px] hidden lg:block">Trang Chủ</p>
                 </div>
-                <Icon.ArrowDown
+              </div>
+              <div className="w-full relative ">
+                <div
+                  className="flex items-center justify-between py-[17px] hover:bg-blue-100 px-[20px]"
+                  onClick={() => setBoxInfo(!BoxInfo)}
+                >
+                  <div className="flex items-center">
+                    <Icon.Card className="fill-current w-[23px] text-gray-400" />
+                    <p className="ml-3 text-[15px] hidden lg:block select-none">
+                      Thông Tin
+                    </p>
+                  </div>
+                  <Icon.ArrowDown
+                    className={
+                      BoxInfo
+                        ? "fill-current w-[12px] text-gray-400 hidden lg:block rotate-180"
+                        : "fill-current w-[12px] text-gray-400  hidden lg:block"
+                    }
+                  />
+                </div>
+                <ul
                   className={
                     BoxInfo
-                      ? "fill-current w-[12px] text-gray-400 hidden lg:block rotate-180"
-                      : "fill-current w-[12px] text-gray-400  hidden lg:block"
+                      ? "block w-[200px] lg:w-full  rounded-r-[3px] lg:rounded-[0px] absolute lg:static top-0 left-[100%] bg-white z-10"
+                      : "hidden "
                   }
-                />
+                >
+                  <li className="">
+                    <NavLink
+                      to="/profile/:id/edit/personal"
+                      className="px-[15px] lg:px-[30px] xl:px-[45px] flex items-center hover:bg-blue-100 py-[13px]"
+                    >
+                      <Icon.User className="fill-current w-[18px] text-gray-400" />
+                      <span className="ml-3 text-[15px] select-none">
+                        Thông Tin Cá Nhân
+                      </span>
+                    </NavLink>
+                  </li>
+                  <li className="px-[15px] lg:px-[30px]  xl:px-[45px] flex items-center hover:bg-blue-100 py-[13px]">
+                    <Icon.Card className="fill-current w-[18px] text-gray-400" />
+                    <span className="ml-3 text-[15px] select-none">
+                      Thông Tin Liên Hệ
+                    </span>
+                  </li>
+                </ul>
               </div>
-              <ul
-                className={
-                  BoxInfo
-                    ? "block w-[200px] lg:w-full  rounded-[3px] lg:rounded-[0px] absolute lg:static top-0 left-[100%] bg-white z-10"
-                    : "hidden "
-                }
-              >
-                <li className="">
-                  <NavLink
-                    to="/profile/:id/edit/personal"
-                    className="px-[15px] lg:px-[30px] xl:px-[45px] flex items-center hover:bg-blue-100 py-[13px]"
-                  >
-                    <Icon.User className="fill-current w-[18px] text-gray-400" />
-                    <span className="ml-3 text-[15px] ">Thông Tin Cá Nhân</span>
-                  </NavLink>
-                </li>
-                <li className="px-[15px] lg:px-[30px]  xl:px-[45px] flex items-center hover:bg-blue-100 py-[13px]">
-                  <Icon.Card className="fill-current w-[18px] text-gray-400" />
-                  <span className="ml-3 text-[15px]">Thông Tin Liên Hệ</span>
-                </li>
-              </ul>
-            </div>
-            <div className="w-full relative">
-              <div
-                className="flex items-center justify-between py-[17px] hover:bg-blue-100 px-[20px]"
-                onClick={() => setBoxSecurity(!BoxSecurity)}
-              >
-                <div className="flex items-center">
-                  <Icon.Shield className="fill-current w-[25px] text-gray-400" />
-                  <p className="ml-3 text-[15px] hidden lg:block">Bảo Mật</p>
+              <div className="w-full relative">
+                <div
+                  className="flex items-center justify-between py-[17px] hover:bg-blue-100 px-[20px]"
+                  onClick={() => setBoxSecurity(!BoxSecurity)}
+                >
+                  <div className="flex items-center">
+                    <Icon.Shield className="fill-current w-[25px] text-gray-400" />
+                    <p className="ml-3 text-[15px] hidden lg:block select-none">
+                      Bảo Mật
+                    </p>
+                  </div>
+                  <Icon.ArrowDown
+                    className={
+                      BoxSecurity
+                        ? "fill-current w-[12px] text-gray-400 hidden lg:block rotate-180 "
+                        : "fill-current w-[12px] text-gray-400 hidden lg:block"
+                    }
+                  />
                 </div>
-                <Icon.ArrowDown
+                <ul
                   className={
                     BoxSecurity
-                      ? "fill-current w-[12px] text-gray-400 hidden lg:block rotate-180 "
-                      : "fill-current w-[12px] text-gray-400 hidden lg:block"
+                      ? " block w-[200px] lg:w-full  rounded-r-[3px] lg:rounded-[0px] absolute lg:static top-0 left-[100%] bg-white z-10"
+                      : " hidden"
                   }
-                />
+                >
+                  <li className="px-[15px] lg:px-[30px] xl:px-[45px] flex items-center justify-center lg:justify-start  hover:bg-blue-100 py-[13px]">
+                    <Icon.Key className="fill-current w-[22px] text-gray-400" />
+                    <span className="ml-3 text-[15px] select-none">
+                      Mật Khẩu
+                    </span>
+                  </li>
+                </ul>
               </div>
-              <ul
-                className={
-                  BoxSecurity
-                    ? " block w-[200px] lg:w-full  rounded-[3px] lg:rounded-[0px] absolute lg:static top-0 left-[100%] bg-white z-10"
-                    : " hidden"
-                }
-              >
-                <li className="px-[15px] lg:px-[30px] xl:px-[45px] flex items-center justify-center lg:justify-start  hover:bg-blue-100 py-[13px]">
-                  <Icon.Key className="fill-current w-[22px] text-gray-400" />
-                  <span className="ml-3 text-[15px]">Mật Khẩu</span>
-                </li>
-              </ul>
             </div>
           </div>
-          <div className="w-full lg:w-[80%] px-[15px] md:px-[30px] ">
+          <div className="w-full lg:w-[80%] px-[15px] md:px-[30px] pt-[75px]">
             <div className=" max-w-[1100px] mx-auto lg:py-[20px] ">
               <div className=" bg-white shadow-lg rounded-[5px] mx-auto">
                 <div className="px-[20px] lg:px-[50px] pt-[15px] pb-[50px]">
@@ -118,7 +130,7 @@ const EditProfile = () => {
                   </div>
                   <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-[30px] gap-y-[15px] mt-10">
                     <div className="bg-gradient-to-r from-[#a1f0f0]  to-[#79a3f1]  text-center pt-5 pb-7 rounded-[5px] cursor-pointer card-item ">
-                      <div className="w-[75px] h-[75px] mx-auto">
+                      <div className=" w-[50px] h-[50px] sm:w-[75px] sm:h-[75px] mx-auto">
                         <img src={profile_icon} alt="" />
                       </div>
                       <p className="text-[20px] md:text-[24px] font-medium mt-3 text-white">
@@ -126,7 +138,7 @@ const EditProfile = () => {
                       </p>
                     </div>
                     <div className="bg-gradient-to-r from-[#a1f0f0]  to-[#79a3f1]  pt-5 pb-7 text-center rounded-[5px] cursor-pointer card-item">
-                      <div className="w-[75px] h-[75px]  mx-auto">
+                      <div className="w-[50px] h-[50px] sm:w-[75px] sm:h-[75px] mx-auto">
                         <img src={contact_icon} alt="" />
                       </div>
                       <p className="text-[20px] md:text-[24px] font-medium mt-3 text-white">
@@ -134,7 +146,7 @@ const EditProfile = () => {
                       </p>
                     </div>
                     <div className="bg-gradient-to-r from-[#a1f0f0]  to-[#79a3f1]  pt-5 pb-7 text-center rounded-[5px] cursor-pointer card-item">
-                      <div className="w-[75px] h-[75px] - mx-auto">
+                      <div className="w-[50px] h-[50px] sm:w-[75px] sm:h-[75px] mx-auto">
                         <img src={key_icon} alt="" />
                       </div>
                       <p className="text-[20px] md:text-[24px] font-medium mt-3 text-white">
