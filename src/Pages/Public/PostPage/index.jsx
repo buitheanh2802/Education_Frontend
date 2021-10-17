b85df7c6c1ae716b3ebaae0b3066821963f15e4d
Nguyễn Thành Đạt <73815905+thanhdat19521@users.noreply.github.com>
2021-10-18 00:39:25 +0700

    update

--- src/Pages/Public/PostPage/index.jsx
+++ src/Pages/Public/PostPage/index.jsx
@@ -1,22 +1,116 @@
-import React from 'react';
-import { Icon } from 'src/Components/Icon';
-import { path } from 'src/Constants/';
-import Navigation from '../Commons/Navigation';
+import React from 'react'
+import Navigation from '../Commons/Navigation'
+import PostView from '../Commons/PostView'
+import FeaturedAuthor from '../Commons/FeaturedAuthor'
+import TrendingTags from '../Commons/TrendingTags'
+import { path } from 'src/Constants/'
+import { Icon } from 'src/Components/Icon'
+import Scrollbar from 'react-smooth-scrollbar'
 
 const PostPage = () => {
 
-    // Navigation
+    // navigation
     const pathName = [
-        { path: path.POSTS, value: "Mới cập nhật" },
-        { path: path.POSTS_POPULAR, value: "Thịnh hành" },
-        { path: path.POSTS_FLOW, value: "Đang theo dõi" },
-        { path: path.POSTS_BOOK_MARK, value: "Bookmark của tôi" },
+        {
+            path: path.QUESTIONS,
+            value: "Mới cập nhật"
+        },
+        {
+            path: path.QUESTIONS_FLOW,
+            value: "Đang theo dõi"
+        },
+        {
+            path: path.QUESTIONS_BOOK_MARK,
+            value: "Bookmark của tôi"
+        }
     ]
-    const button = { path: path.POSTS_CREATE, icon: Icon.Pen, value: "Viết bài" }
+    const button = { path: path.QUESTIONS_CREATE, icon: Icon.questions, value: "Đặt câu hỏi" }
+
+    // authors
+    const authors = [
+        {
+            path: "/",
+            fullname: "Nguyễn Thành Đạt",
+            username: "@nguyenthanhdat",
+            avatar: "https://images.viblo.asia/avatar/afc7299e-8b69-48e5-a2e4-8bd52b38123e.jpg",
+            point: 567,
+            question: 234,
+            folow: 345
+        }
+    ]
+
+    // tags
+    const tags = [
+        {
+            path: "/",
+            value: "NodeJS"
+        }
+    ]
+
+
+    // Questions
+    const fieldQuestions = [
+        {
+            user: {
+                fullname: "Nguyễn Thành Đạt",
+                avatar: "https://images.viblo.asia/avatar/afc7299e-8b69-48e5-a2e4-8bd52b38123e.jpg",
+                path: "/profile/nguyenthanhdat"
+            },
+            post: {
+                title: "Tìm hiểu về middleware trong NodeJS Tìm hiểu về middleware trong NodeJS Tìm hiểu về middleware trong NodeJS ",
+                time: "Khoảng 2h trước",
+                view: 150,
+                comment: 87,
+                bookmark: 25,
+                link: 120,
+                dislike: 20,
+                path: path.POSTS_ID,
+                tags: [
+                    { path: path.TAGS_ID, value: "NodeJS" },
+                    { path: path.TAGS_ID, value: "ExpressJS" }
+                ]
+            }
+        },
+        {
+            user: {
+                fullname: "Bùi Thế Anh",
+                avatar: "https://images.viblo.asia/avatar/afc7299e-8b69-48e5-a2e4-8bd52b38123e.jpg",
+                path: "/profile/buitheanh"
+            },
+            post: {
+                title: "Tìm hiểu về ExpressJS",
+                time: "Khoảng 3h trước",
+                view: 150,
+                comment: 87,
+                bookmark: 25,
+                link: 120,
+                dislike: 20,
+                path: path.POSTS_ID,
+                tags: [
+                    { path: path.TAGS_ID, value: "ExpressJS" }
+                ]
+            }
+        }
+    ]
 
     return (
-        <div className="mt-[55px] container mx-auto py-[20px]">
+        <div className="container mx-auto mt-[55px] py-[20px]">
             <Navigation path={pathName} button={button} />
+            <div className="grid grid-cols-10 gap-[20px] mt-[20px]">
+                <Scrollbar className="col-span-10 lg:col-span-7 shadow-sm bg-white px-[5px] rounded h-screen">
+                    <PostView data={fieldQuestions} />
+                    <PostView data={fieldQuestions} />
+                    <PostView data={fieldQuestions} />
+                    <PostView data={fieldQuestions} />
+                    <PostView data={fieldQuestions} />
+                    <PostView data={fieldQuestions} />
+                    <PostView data={fieldQuestions} />
+                </Scrollbar>
+                <Scrollbar className="col-span-10 lg:col-span-3 bg-white shadow rounded h-screen">
+                    <FeaturedAuthor authors={authors} />
+                    <TrendingTags tags={tags} />
+                </Scrollbar>
+            </div>
         </div>
     )
 }
