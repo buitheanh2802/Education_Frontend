0dccf080cffa4351a694fe14a120172a1cf1e425
Nguyễn Thành Đạt <73815905+thanhdat19521@users.noreply.github.com>
2021-11-04 04:01:53 +0700

    updaet

--- src/Pages/Public/QuizPage/index.jsx
+++ src/Pages/Public/QuizPage/index.jsx
@@ -12,19 +12,6 @@ const QuizPage = () => {
     return (
         <div className="container mx-auto mt-[55px] py-[20px]">
             <Navigation path={pathName} button={button} />
-            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-[20px] mt-[20px]">
-                <div className="shadow-sm bg-white rounded  course-item p-[15px] relative" >
-                    <div className="w-full h-[220px] bg-no-repeat bg-cover   bg-center rounded  cursor-pointer" style={{ backgroundImage: `url(https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQK2yuHuE_KiWXOfDSMKzBFbAFiolnyJ9gtbQ&usqp=CAU)` }}> </div>
-                    <div className="w-full" >
-                        <div className="flex justify-between items-center mt-[12px]">
-                            <span className="text-[20px] font-bold  cursor-pointer">Hoc thiet ke giao dien don gian</span>
-                        </div>
-                        <p className="text-[16px] leading-[24px] mt-[4px] text-blue-500">
-                            Giup ban ren luyen cac ky nang code Html, Css
-                        </p>
-                    </div>
-                </div>
-            </div>
         </div>
     )
 }
