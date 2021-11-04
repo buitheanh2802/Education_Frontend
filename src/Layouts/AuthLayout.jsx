50cebeee57393faf83ae0e4105fc7bc6fc65bbe4
Bui The Anh <bupbui95@gmail.com>
2021-11-05 01:15:32 +0700

    merge brand hanhnguyen

--- src/Layouts/AuthLayout.jsx
+++ src/Layouts/AuthLayout.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
-import { Route, Switch } from 'react-router';
 import { path } from '../Constants';
 import Login from '../Pages/Auth/Login';
 import Register from '../Pages/Auth/Register';
+import { useSelector } from 'react-redux';
+import { Route, Switch, Redirect } from 'react-router';
 
 const AuthLayout = () => {
+    const { profile } = useSelector(state => state.Auth)
+
+    if (profile) return <Redirect to={path.HOME} />
     return (
         <Switch>
             <Route path={path.LOGIN} component={Login} />
