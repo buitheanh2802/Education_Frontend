b98950667e92f9b31d162c0d84e62ca038544aa0
Nguyễn Thành Đạt <thanhdat19521@gmail.com>
2021-11-05 16:24:17 +0700

    updaet

--- src/Pages/Public/DetailChallenge/index.jsx
+++ src/Pages/Public/DetailChallenge/index.jsx
@@ -1,14 +1,20 @@
 import React, { useEffect, useState } from 'react'
 import { useDispatch } from 'react-redux';
 import { useParams } from 'react-router'
-import Loading from 'src/Components/Loading';
+import { path } from 'src/Constants/';
 import { ActionGetChallenge } from 'src/Redux/Actions/Challenge.action';
+import Navigation from '../Commons/Navigation';
 
 const DetailChallenge = () => {
     const dispatch = useDispatch();
     const [isLoading, setIsLoading] = useState(true);
     const [challenge, setChallenge] = useState(null);
     const { id } = useParams();
+
+    const pathName = [
+        { path: `${path.CHALLENGE}/detail/${id}`, value: "Chi tiết bài tập" },
+        { path: `${path.CHALLENGE}/detail/${id}`, value: "Giải pháp" }
+    ]
 
     useEffect(() => {
         (async () => {
@@ -21,14 +27,13 @@ const DetailChallenge = () => {
         })()
     }, [dispatch, id])
 
-    console.log(challenge)
-
     return (
         <div className="container mx-auto mt-[55px] py-[20px]">
-            {isLoading ? <Loading className="w-[40px] h-[40px] fill-current text-gray-500 mx-auto mt-[20px]" /> :
+            <Navigation path={pathName} />
+            {/* {isLoading ? <Loading className="w-[40px] h-[40px] fill-current text-gray-500 mx-auto mt-[20px]" /> :
                 <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-[20px] mt-[20px]">
                     chi tiết
-                </div>}
+                </div>} */}
         </div>
     )
 }
