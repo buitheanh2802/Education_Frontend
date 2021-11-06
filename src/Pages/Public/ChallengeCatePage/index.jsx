b7c2c6ea481c2feef6c3c678dbea865de3006ed8
Nguyễn Thành Đạt <73815905+thanhdat19521@users.noreply.github.com>
2021-11-06 14:48:16 +0700

    updaet

--- src/Pages/Public/ChallengeCatePage/index.jsx
+++ src/Pages/Public/ChallengeCatePage/index.jsx
@@ -23,38 +23,73 @@ const ChallengeCatePage = () => {
     return (
         <div className="container mx-auto mt-[55px] py-[20px]">
             <Navigation path={pathName} />
-            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-[20px] mt-[20px]">
-                {isLoading && (
-                    <>
-                        {[...Array(4)].map((item, index) => {
-                            return (
-                                <div key={index} className="shadow-sm duration-300 bg-white rounded  course-item p-[15px] relative border">
-                                    <div className="w-full h-[200px] sm:h-[220px] xl:h-[200px] bg-no-repeat bg-cover bg-center rounded cursor-pointer">
-                                        <Skeleton className="z-0 h-full" />
-                                    </div>
-                                    <div className="w-full mt-[12px]">
-                                        <Skeleton className="z-0 w-full py-[5px]" />
-                                    </div>
-                                    <Skeleton />
-                                    <Skeleton className="z-0 w-2/3" />
-                                </div>
-                            )
-                        })}
-                    </>
-                )}
-                {challengeCates?.map(item => {
-                    return (
-                        <div key={item?._id} className="shadow-sm hover:shadow-md duration-300 bg-white rounded  course-item p-[15px] relative border" >
-                            <div onClick={() => history.push(path.CHALLENGE + "/" + item?._id)} className="w-full h-[220px] bg-no-repeat bg-cover  bg-center rounded  cursor-pointer" style={{ backgroundImage: `url(${item?.avatar})` }}> </div>
-                            <div className="w-full" >
-                                <div className="flex justify-between items-center mt-[12px]">
-                                    <Link to={path.CHALLENGE + "/" + item?._id}><span className="text-[20px] font-bold  cursor-pointer text-gray-800 hover:text-blue-600">{item?.title}</span></Link>
-                                </div>
-                                <p className="text-[16px] leading-[24px] mt-[4px] text-gray-500"> {item?.descriptions} </p>
+            <div className="mt-[25px]">
+                <h3 className="text-[20px] font-medium text-blue-900">Bắt đầu thực hành với devstar</h3>
+                <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-[20px] mt-[10px]">
+                    <div className="shadow-sm hover:shadow-md duration-300 bg-white rounded  course-item p-[25px] relative border" >
+                        <div className="w-full h-[220px] bg-no-repeat bg-cover  bg-center rounded  cursor-pointer" style={{ backgroundImage: `url(https://images.ctfassets.net/2nst5tn93zo3/5cKTkp5Cm6DLTAGUKiwJcO/056972edf8bc341e58716e658074b1e6/Step1.svg)` }}> </div>
+                        <div className="w-full" >
+                            <div className="flex justify-between items-center mt-[12px]">
+                                <span className="text-[20px] font-bold  cursor-pointer text-gray-800 hover:text-blue-600">Bước 1: Chọn một thử thách</span>
                             </div>
+                            <p className="text-[16px] leading-[24px] mt-[4px] text-gray-500"> Chọn một thử thách, đọc kỹ mô tả. Đường dẫn giúp bạn điều hướng và bạn có thể kiếm được chứng chỉ sau khi hoàn thành tất cả các thử thách trong đường dẫn </p>
                         </div>
-                    )
-                })}
+                    </div>
+                    <div className="shadow-sm hover:shadow-md duration-300 bg-white rounded  course-item p-[25px] relative border" >
+                        <div className="w-full h-[220px] bg-no-repeat bg-cover  bg-center rounded  cursor-pointer" style={{ backgroundImage: `url(https://images.ctfassets.net/2nst5tn93zo3/4EKznKCFPvWUKXakeHZP5R/0a9418f303fb4e975dc7a1368a71fdb1/Step2.svg)` }}> </div>
+                        <div className="w-full" >
+                            <div className="flex justify-between items-center mt-[12px]">
+                                <span className="text-[20px] font-bold  cursor-pointer text-gray-800 hover:text-blue-600">Bước 2: Làm việc một mình hoặc với một nhóm</span>
+                            </div>
+                            <p className="text-[16px] leading-[24px] mt-[4px] text-gray-500"> Tất cả các thử thách có thể được hoàn thành bởi một người, nhưng tinh thần đồng đội là rất quan trọng tại nơi làm việc. Nếu thử thách quá lớn đối với bạn, hãy rủ một người bạn cùng tham gia.</p>
+                        </div>
+                    </div>
+                    <div className="shadow-sm hover:shadow-md duration-300 bg-white rounded  course-item p-[25px] relative border" >
+                        <div className="w-full h-[220px] bg-no-repeat bg-cover  bg-center rounded  cursor-pointer" style={{ backgroundImage: `url(https://images.ctfassets.net/2nst5tn93zo3/roJRJhK1F5HBrv8hTTxxC/8f9719b9da0788040f5de8acaa0dbc30/Step3.svg)` }}> </div>
+                        <div className="w-full" >
+                            <div className="flex justify-between items-center mt-[12px]">
+                                <span className="text-[20px] font-bold  cursor-pointer text-gray-800 hover:text-blue-600">Bước 3: Gửi các giải pháp của bạn và đưa ra phản hồi</span>
+                            </div>
+                            <p className="text-[16px] leading-[24px] mt-[4px] text-gray-500"> Khi bạn hoàn thành thử thách, hãy gửi giải pháp của bạn. Bạn cũng có thể kiểm tra xem có giải pháp nào hiện có hay không và cung cấp cho họ một số phản hồi. </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="mt-[25px]" >
+                <h3 className="text-[20px] font-medium text-blue-900">Các loại bài tập</h3>
+                <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-[20px] mt-[10px]">
+                    {isLoading &&
+                        <>
+                            {[...Array(3)].map((item, index) => {
+                                return (
+                                    <div key={index} className="shadow-sm duration-300 bg-white rounded  course-item p-[25px] relative border">
+                                        <div className="w-full h-[200px] sm:h-[220px] xl:h-[200px] bg-no-repeat bg-cover bg-center rounded cursor-pointer">
+                                            <Skeleton className="z-0 h-full" />
+                                        </div>
+                                        <div className="w-full mt-[12px]">
+                                            <Skeleton className="z-0 w-full py-[5px]" />
+                                        </div>
+                                        <Skeleton />
+                                        <Skeleton className="z-0 w-2/3" />
+                                    </div>
+                                )
+                            })}
+                        </>
+                    }
+                    {challengeCates?.map(item => {
+                        return (
+                            <div key={item?._id} className="shadow-sm hover:shadow-md duration-300 bg-white rounded  course-item p-[25px] relative border" >
+                                <div onClick={() => history.push(path.CHALLENGE + "/" + item?._id)} className="w-full h-[220px] bg-no-repeat bg-cover  bg-center rounded  cursor-pointer" style={{ backgroundImage: `url(${item?.avatar})` }}> </div>
+                                <div className="w-full" >
+                                    <div className="flex justify-between items-center mt-[12px]">
+                                        <Link to={path.CHALLENGE + "/" + item?._id}><span className="text-[20px] font-bold  cursor-pointer text-gray-800 hover:text-blue-600">{item?.title}</span></Link>
+                                    </div>
+                                    <p className="text-[16px] leading-[24px] mt-[4px] text-gray-500"> {item?.descriptions} </p>
+                                </div>
+                            </div>
+                        )
+                    })}
+                </div>
             </div>
         </div>
     )
