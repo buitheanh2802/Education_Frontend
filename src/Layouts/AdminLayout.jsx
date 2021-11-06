41c8ea1860d306ae001aaa2d73deea230d1d12e4
Nguyễn Thành Đạt <73815905+thanhdat19521@users.noreply.github.com>
2021-11-06 13:25:20 +0700

    update

--- src/Layouts/AdminLayout.jsx
+++ src/Layouts/AdminLayout.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import { Switch } from 'react-router'
+import Dashboard from 'src/Pages/Private/Dashboard'
+import PrivateRouter from 'src/Routes/PrivateRouter'
+import { path } from '../Constants'
 
 const AdminLayout = () => {
     return (
-        <div>
-            xin chào nguyễn thành đạt
-        </div>
+        <Switch>
+            <PrivateRouter exact path={path.ADMIN} component={Dashboard}/>
+        </Switch>
     )
 }
 
