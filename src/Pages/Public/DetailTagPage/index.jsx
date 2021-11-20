6d2bff2bb090ff9fc50b02ce59c4758a3884651a
Kieu Hanh Nguyen <kieuhanhnguyen01@gmail.com>
2021-11-20 17:14:14 +0700

    dtt

--- src/Pages/Public/DetailTagPage/index.jsx
+++ src/Pages/Public/DetailTagPage/index.jsx
@@ -2,23 +2,24 @@ import React, { useState, useEffect } from 'react'
 import { Icon } from 'src/Components/Icon'
 import DetailTagView from '../Commons/DetailTagView'
 import FeaturedTag from '../Commons/FeaturedTag'
-import NavigationInDetailTag from '../Commons/NavigationInDetailTag'
 import TagAPi from 'src/Apis/TagApi'
 import { useParams } from "react-router-dom";
 import FollowApi from 'src/Apis/FollowApi';
 import { useDispatch } from "react-redux";
 import { setLoading } from "src/Redux/Slices/Loading.slice";
-import { Switch, Route } from "react-router-dom";
 import FollowerTag from '../Commons/DetailTagView/FollowerTag'
 
 const DetailTagPage = () => {
     const { slug } = useParams();
     const [tag, setTag] = useState([]);
     const [tags, setTags] = useState({});
-    const [postTag, setPostTag] = useState([]);
-    const [questionTag, setQuestionTag] = useState([]);
-    const [follower, setFollower] = useState([]);
     const dispatch = useDispatch();
+    const [dataTag, setDataTag] = useState({
+        posts: [],
+        questions: [],
+        followers: []
+    });
+    const [tab, setTab] = useState(1)
 
     useEffect(() => {
         const detailTag = async () => {
@@ -40,39 +41,26 @@ const DetailTagPage = () => {
             }
         };
         popTag();
-
-        const postTag = async () => {
-            try {
-                const { data: posts } = await TagAPi.getPostInTag(slug);
-                setPostTag(posts.data.models);
-            } catch (error) {
-                console.log(error);
-            }
-        };
-        postTag();
-
-        const questionTag = async () => {
-            try {
-                const { data: questions } = await TagAPi.getQuestionInTag(slug);
-                setQuestionTag(questions.data.models);
-            } catch (error) {
-                console.log(error);
-            }
-        };
-        questionTag();
-
-        const followerTag = async () => {
-            try {
-                const { data: follower } = await TagAPi.getFollowInTag(slug);
-                setFollower(follower.data.models);
-                console.log(follower.data.models);
-
-            } catch (error) {
-                console.log(error);
-            }
-        };
-        followerTag();
     }, []);
+
+    useEffect(() => {
+        Promise.all([
+            TagAPi.getPostInTag(slug),
+            TagAPi.getQuestionInTag(slug),
+            TagAPi.getFollowInTag(slug)
+        ]).then(data => {
+            const postTags = data[0].data.data.models;
+            const questions = data[1].data.data.models;
+            const followers = data[2].data.data.models;
+            setDataTag({
+                posts: postTags,
+                questions: questions,
+                followers: followers
+            })
+        }).catch(error => {
+            console.log('error')
+        })
+    }, [slug])
 
     const handleFollow = async (id) => {
         dispatch(setLoading(true))
@@ -82,23 +70,23 @@ const DetailTagPage = () => {
             dispatch(setLoading(false))
         } else {
             await FollowApi.followTag(id);
-            setTag({...tag, isFollowing: true})
+            setTag({ ...tag, isFollowing: true })
             dispatch(setLoading(false))
         }
     }
 
-    const pathName = [
+    const path = [
         {
-            path: `/tag/${slug}`,
-            value: "Bài viết"
+            label: "Bài viết",
+            value: 1
         },
         {
-            path: `/tag/${slug}/question`,
-            value: "Câu hỏi"
+            label: "Câu hỏi",
+            value: 2
         },
         {
-            path: `/tag/${slug}/follower`,
-            value: "Người theo dõi"
+            label: "Người theo dõi",
+            value: 3
         }
     ]
 
@@ -115,42 +103,36 @@ const DetailTagPage = () => {
                                 <h3 className="text-[30px] font-semibold inline-block">{tag?.name}</h3>
                                 <Icon.Star className="w-[20px] mb-[15px] ml-[10px] inline-block" />
                             </div>
-                            {tag?.isFollowing ? 
-                            <button onClick={() => handleFollow(tag?._id)} className="mt-[10px] bg-[#0d61c7] border border-[#0d61c7] hover:bg-[#fff] hover:text-[#0d61c7] text-[#fff] rounded md:px-[10px] md:py-[5px] md:text-[14px] px-[10px] py-[5px] sm:text-[14px] lg:px-[8px] lg:py-[5px] lg:text-[10px] xl:px-[8px] xl:py-[5px] xl:text-[14px] ">
-                                 - Bỏ theo dõi
-                            </button>
-                            : 
-                            <button onClick={() => handleFollow(tag?._id)} className="mt-[10px] bg-[#fff] border border-[#0d61c7] hover:bg-[#0d61c7] hover:text-[#BEE3F8] text-[#0d61c7] rounded md:px-[10px] md:py-[5px] md:text-[14px] px-[10px] py-[5px] sm:text-[14px] lg:px-[8px] lg:py-[5px] lg:text-[10px] xl:px-[8px] xl:py-[5px] xl:text-[14px] ">
-                                 + theo dõi
-                            </button>
-                            }   
+                            {tag?.isFollowing ?
+                                <button onClick={() => handleFollow(tag?._id)} className="mt-[10px] bg-[#0d61c7] border border-[#0d61c7] hover:bg-[#fff] hover:text-[#0d61c7] text-[#fff] rounded md:px-[10px] md:py-[5px] md:text-[14px] px-[10px] py-[5px] sm:text-[14px] lg:px-[8px] lg:py-[5px] lg:text-[10px] xl:px-[8px] xl:py-[5px] xl:text-[14px] ">
+                                    - Bỏ theo dõi
+                                </button>
+                                :
+                                <button onClick={() => handleFollow(tag?._id)} className="mt-[10px] bg-[#fff] border border-[#0d61c7] hover:bg-[#0d61c7] hover:text-[#BEE3F8] text-[#0d61c7] rounded md:px-[10px] md:py-[5px] md:text-[14px] px-[10px] py-[5px] sm:text-[14px] lg:px-[8px] lg:py-[5px] lg:text-[10px] xl:px-[8px] xl:py-[5px] xl:text-[14px] ">
+                                    + theo dõi
+                                </button>
+                            }
                         </div>
                     </div>
                     <div className="w-full shadow-sm bg-white rounded">
-                        <NavigationInDetailTag path={pathName} />
-                        <Switch>
-                            <Route
-                                exact
-                                path={`/tag/${slug}`}
-                                render={(props) => (
-                                <DetailTagView data={postTag} {...props} />
-                                )}>
-                            </Route>
-                            <Route
-                                exact
-                                path={`/tag/${slug}/question`}
-                                render={(props) => (
-                                <DetailTagView data={questionTag} {...props} />
-                                )}>
-                            </Route>
-                            <Route
-                                exact
-                                path={`/tag/${slug}/follower`}
-                                render={(props) => 
-                                <FollowerTag follower={follower} {...props} /> 
-                                }>
-                            </Route>
-                        </Switch>
+                        <div className="md:flex md:justify-between sm:grid sm:grid-cols-1 shadow-sm bg-white px-[5px] rounded">
+                            <div className=" py-[15px] flex items-center">
+                                {path?.map((item, index) => (
+                                    <p key={index} onClick={() => setTab(item.value)}
+                                        className={item.value === tab ? "after:absolute after:w-full after:h-[2px] after:rounded after:bottom-[-16px] after:left-0 after:bg-[#1273eb] font-medium text-black relative text-[12px] sm:text-[15px] px-[0px] sm:px-[5px] text-gray-600 hover:text-blue-600 sm:mr-[20px] mr-[15px]" : "relative text-[12px] sm:text-[15px] px-[0px] sm:px-[5px] text-gray-600 hover:text-blue-600 sm:mr-[20px] mr-[15px]"} >
+                                        {item?.label}
+                                    </p>
+                                ))}
+                            </div>
+                            <div className="self-center">
+                                <select name="" className="border border-[#707885] rounded px-[10px] py-[5px]">
+                                    <option value="">Tất cả</option>
+                                    <option value="">Xem nhiều nhất</option>
+                                    <option value="">Mới nhất</option>
+                                </select>
+                            </div>
+                        </div>
+                        {tab !== 3 ? <DetailTagView data={ tab === 1 ? dataTag.posts : dataTag.questions } /> : <FollowerTag follower={dataTag.followers} />}
                     </div>
                 </div>
                 <div className=" min-w-100 max-w-100 bg-white shadow rounded">
