440bc6543032f1c49007cf6560dabd75d5ee5c10
Kieu Hanh Nguyen <kieuhanhnguyen01@gmail.com>
2021-12-08 21:33:22 +0700

    fixadmintag

--- src/Pages/Private/TagManager/components/publish-item.jsx
+++ src/Pages/Private/TagManager/components/publish-item.jsx
@@ -1,12 +1,31 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { Link } from "react-router-dom";
+import LoadingIcon from "src/Components/Loading/LoadingIcon";
+import { useSelector } from "react-redux";
 
-const PublishItem = () => {
-
+const PublishItem = (props) => {
+    const { name, index, follow, post, question, id, slug } = props;
+    const { profile } = useSelector((state) => state.Auth);
+    const [loading, setLoading] = useState({
+        error: false,
+        success: false,
+    });
 
     return (
         <div className="nav bg-white border-b  text-[15px] p-[10px] grid 
-        grid-cols-[60px,1fr,2.5fr,0.5fr,0.5fr,1.2fr] ">
-           
+        grid-cols-[60px,1.5fr,1fr,0.75fr,0.75fr,1.5fr]">
+            <div className="font-medium ">{index}</div>
+            <div className="font-medium mr-[30px] text-blue-500 underline ">
+                <Link to={`/tag/${slug}}`}>{name}</Link>
+            </div>
+            <div className="text-[13px]">{follow}</div>
+            <div className="text-[13px]">{post}</div>
+            <div className="text-[13px]">{question}</div>
+            <div className="text-center">
+                <button className="px-[15px] py-[5px] text-white bg-red-500 rounded-md flex mx-auto" >
+                    Xóa
+                </button>
+            </div>
         </div>
     );
 };
--- src/Pages/Private/TagManager/index.jsx
+++ src/Pages/Private/TagManager/index.jsx
@@ -2,15 +2,57 @@ import React, { useEffect, useState } from "react";
 import { Link, useLocation } from "react-router-dom";
 import Header from "./components/header";
 import PublishNav from "./components/publish-nav";
+import PublishItem from "./components/publish-item";
+import queryString from "query-string";
+import SkeletonGroup from "./components/skeleton-group";
+import TagAPi from "src/Apis/TagApi";
 
-const TagManager = () => {
+const TagManager = (props) => {
+    const query = queryString.parse(props.location.search, {
+        parseNumbers: true,
+    });
+    const [startCall, setStartCall] = useState(false);
+    const [tagList, setTagList] = useState([]);
+    useEffect(() => {
+        const getData = async () => {
+            try {
+                setStartCall(true);
+                const { data: tagLists} = await TagAPi.getAll();
+                setTagList(tagLists.data.models);
+                setStartCall(false);
+            } catch (error) {
+                console.log(error);
+                setStartCall(false);
+            }
+        }
+        getData();
+    }, []);
+
     return (
         <div className="w-full">
             <Header />
             <PublishNav />
-            
+            {startCall && <SkeletonGroup />}
+            {tagList &&
+                tagList.map((item, index) => {
+                    return (
+                        <PublishItem
+                            index={index + 1}
+                            key={item._id}
+                            id={item._id}
+                            name={item.name}
+                            follow={item.followerCounts}
+                            post={item.postCounts}
+                            question={item.questionCounts}
+                            slug={item.slug}
+                        />
+                    );
+                })}
         </div>
-    )
+    );
 };
 
 export default TagManager;
+
+
+
