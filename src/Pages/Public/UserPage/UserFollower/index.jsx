0918c811e07b999f32a3e5257ee6279a8649dc91
Kieu Hanh Nguyen <kieuhanhnguyen01@gmail.com>
2021-12-12 12:13:06 +0700

    fix follow

--- src/Pages/Public/UserPage/UserFollower/index.jsx
+++ src/Pages/Public/UserPage/UserFollower/index.jsx
@@ -78,7 +78,7 @@ const UserFollower = (props) => {
                     <div className="gap-x-[20px] grid lg:grid-cols-3 md:grid-cols-2">
                         {userFollowers?.map((item, index) => {
                             return (
-                                <div key={index} className="flex items-center py-[20px] px-[10px] mb-[20px] bg-white">
+                                <div key={index} className="flex items-center py-[20px] px-[10px] my-[5px] bg-white">
                                     <div className="">
                                         {item?.avatar?.avatarUrl ?
                                             <img
@@ -127,8 +127,10 @@ const UserFollower = (props) => {
                                         </div>
                                     </div>
                                     {item?.username === profile?.username ?
-                                        <div>
-
+                                        <div className="mx-[10px] ">
+                                            <button onClick={() => history.push('/profile/me')} className="mt-[10px] bg-[#0d61c7] border border-[#0d61c7] hover:bg-[#fff] hover:text-[#0d61c7] text-[#fff] rounded md:px-[10px] md:py-[5px] md:text-[14px] px-[10px] py-[5px] sm:text-[14px] lg:px-[8px] lg:py-[5px] lg:text-[10px] xl:px-[8px] xl:py-[5px] xl:text-[14px] ">
+                                                Xem thông tin
+                                            </button>
                                         </div>
                                         :
                                         item?.isFollowing ? (
