b5a96c04e5f72adfdb50054f52bac1548852dede
Nguyễn Thành Đạt <73815905+thanhdat19521@users.noreply.github.com>
2021-12-19 10:40:06 +0700

    updaet

--- src/Pages/Private/CommentManager/components/skeleton-group.jsx
+++ src/Pages/Private/CommentManager/components/skeleton-group.jsx
@@ -8,7 +8,10 @@ const SkeletonGroup = () => {
                 return (
                     <div className="bg-white py-[10px] px-[5px] grid grid-cols-[65px,1fr,4fr,1fr,0.5fr] items-center">
                         <div className="mr-5" ><Skeleton duration={1} /></div>
-                        <div className="mr-5" ><Skeleton duration={1} /></div>
+                        <div className="mr-5" >
+                            <Skeleton duration={1} />
+                            <Skeleton className='w-1/2' duration={1} />
+                        </div>
                         <div className="mr-5" >
                             <Skeleton duration={1} />
                             <Skeleton duration={1} />
